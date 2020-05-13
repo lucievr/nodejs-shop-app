94e3b2bf675af0280cbf1ec3a16c56678673ab47
lucievr <lucy.vrsovska@gmail.com>
2020-05-14 00:23:14 +0200

    Add static file with style for Shop and Add Product pages

--- 02-node-with-express/app.js
+++ 02-node-with-express/app.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 // parses the body of the request and calls next() under the hood (data sent through a form, not all types of data)
 
+app.use(express.static(path.join(__dirname, 'public'))); // for serving static files
+
 // app.use((req, res, next) => { // allows us to add a new middleware function which will be executed for every incoming request
 //  console.log('This always runs');
 //  next(); // next needs to be called here to allow the request to continue to the next middleware function

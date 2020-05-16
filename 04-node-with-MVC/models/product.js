be7b789d51fd7158cfb4e2a06b6f76c0194bff70
lucievr <lucy.vrsovska@gmail.com>
2020-05-17 01:19:54 +0200

    Create MVC structure for project

---
+++ 04-node-with-MVC/models/product.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = (callback) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return callback([]);
+    }
+    callback(JSON.parse(fileContent));
+  });
+};
+
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+
+  save() {
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(callback) {
+    // static ensures this method can be called directly on the object itself, not just instantiated object
+    getProductsFromFile(callback);
+  }
+};

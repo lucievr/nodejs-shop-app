c39403610d3b17d03c04d47d434c1a5f9ffb895d
lucievr <lucy.vrsovska@gmail.com>
2020-05-16 23:58:24 +0200

    Create templates using EJS

--- 03-dynamic-data-template/app.js
+++ 03-dynamic-data-template/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const expressHbs = require('express-handlebars');
+// const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -9,9 +9,11 @@ const shopRoutes = require('./routes/shop');
 const app = express();
 
 // app.engine('hbs', expressHbs({ defaultLayout: '' })); // initialise express handlebars engine, with no layout
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}))
-app.set('view engine', 'hbs');
+// app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}))
+// app.set('view engine', 'hbs');
 // app.set('view engine', 'pug'); // setting templating engine to Pug
+
+app.set('view engine', 'ejs');
 app.set('views', 'views'); // pointing to where the views folder is (redundant as it is /views)
 
 app.use(bodyParser.urlencoded({ extended: false }));

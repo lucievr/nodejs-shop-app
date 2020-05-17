22d7c3bacde1768ce3f33f83c8bd918f1cc10c06
lucievr <lucy.vrsovska@gmail.com>
2020-05-18 01:11:38 +0200

    Add product detail page and pass id as params

---
+++ 05-dynamic-routes-with-params/controllers/shop.js
@@ -0,0 +1,58 @@
+const Product = require('../models/product');
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render('shop/product-list', {
+      prods: products,
+      docTitle: 'My Shop',
+      path: '/products',
+    });
+  });
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    res.render('shop/product-detail', {
+      product: product,
+      docTitle: product.title,
+      path: '/products' // mark /products as active while on prod detail page
+    });
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render('shop/index', {
+      prods: products,
+      docTitle: 'My Shop',
+      path: '/',
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/cart',
+    docTitle: 'Your Cart',
+  });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId; // needs to match the name of the hidden input
+  res.redirect('/cart');
+}
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    docTitle: 'Your Orders',
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    docTitle: 'Checkout',
+  });
+};
---
+++ 05-dynamic-routes-with-params/models/product.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = (callback) => {
+  fs.readFile(p, (err, fileContent) => {
+    // when reading the file at p path location we either get an error or the data
+    if (err) {
+      return callback([]);
+    }
+    callback(JSON.parse(fileContent));
+  });
+};
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    this.id = (Math.random().toFixed(2) * 100).toString();
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(callback) {
+    // callback as an argument, callback will be executed once fetchAll is done
+    // static ensures this method can be called directly on the object itself, not just instantiated object
+    getProductsFromFile(callback);
+  }
+
+  static findById(id, callback) {
+    getProductsFromFile((products) => {
+      const product = products.find(p => p.id === id);
+      callback(product);
+    });
+  }
+};

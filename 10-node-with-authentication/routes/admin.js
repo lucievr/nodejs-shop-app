f2ef5aa006310b26f0e512b0d8b7358f8e830dae
lucievr <lucy.vrsovska@gmail.com>
2020-05-25 00:29:56 +0200

    Add protected routes from unauthenticated access

--- 10-node-with-authentication/routes/admin.js
+++ 10-node-with-authentication/routes/admin.js
@@ -1,19 +1,21 @@
 const express = require('express');
 
 const adminController = require('../controllers/admin');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
-router.get('/add-product', adminController.getAddProduct);
+// first goes through isAuth middleware (if true next() is called and getAddProduct will be executed next)
+router.get('/add-product', isAuth, adminController.getAddProduct);
 
-router.get('/products', adminController.getProducts);
+router.get('/products', isAuth, adminController.getProducts);
 
-router.post('/add-product', adminController.postAddProduct);
+router.post('/add-product', isAuth, adminController.postAddProduct);
 
-router.get('/edit-product/:productId', adminController.getEditProduct);
+router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
-router.post('/edit-product', adminController.postEditProduct);
+router.post('/edit-product', isAuth, adminController.postEditProduct);
 
-router.post('/delete-product', adminController.postDeleteProduct);
+router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
 module.exports = router;

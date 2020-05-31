f05617a363f38f0c12224fd1933b8bc083c7a230
lucievr <lucy.vrsovska@gmail.com>
2020-05-31 05:28:16 +0200

    Add basic payments with Stripe

--- 15-node-with-payments/controllers/shop.js
+++ 15-node-with-payments/controllers/shop.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
+
 const fs = require('fs');
 const path = require('path');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const PDFDocument = require('pdfkit');
 
@@ -140,23 +143,77 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.getCheckout = (req, res, next) => {
+  let products;
+  let total;
   req.user
-  .populate('cart.items.productId')
-  .execPopulate()
-  .then((user) => {
-    const products = user.cart.items;
-    res.render('shop/checkout', {
-      path: '/checkout',
-      docTitle: 'Checkout',
-      products: products,
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then((user) => {
+      products = user.cart.items;
+      products.forEach((prod) => {
+        total += prod.quantity * prod.productId.price;
+      });
+      return stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: products.map((prod) => {
+          return {
+            name: prod.productId.title,
+            description: prod.productId.description,
+            amount: prod.productId.price * 100, // in cents
+            currency: 'usd',
+            quantity: prod.quantity,
+          };
+        }),
+        success_url: req.protocol + '://' + req.get('host') + '/checkout/success', // http://localhost:3000/checkout/success
+        cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel',
+      });
+    })
+    .then((session) => {
+      res.render('shop/checkout', {
+        path: '/checkout',
+        docTitle: 'Checkout',
+        products: products,
+        totalSum: total,
+        sessionId: session.id,
+      });
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
-  })
-  .catch((err) => {
-    const error = new Error(err);
-    error.httpStatusCode = 500;
-    return next(error);
-  });
-}
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
+  req.user
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then((user) => {
+      const products = user.cart.items.map((item) => {
+        // needs to be mapped to match the data structure in orderSchema
+        return { quantity: item.quantity, product: { ...item.productId._doc } }; // _doc we pull out all the product data stored in the document
+      });
+      const order = new Order({
+        user: {
+          email: req.user.email,
+          userId: req.user, // can use the entire object and mongoose will pick the id from it
+        },
+        products: products,
+      });
+      return order.save();
+    })
+    .then((result) => {
+      return req.user.clearCart();
+    })
+    .then(() => {
+      res.redirect('/orders');
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
 
 exports.postOrder = (req, res, next) => {
   req.user

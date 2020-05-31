ca2367b71d77c69d62b4dd742aecc202a264d0e0
lucievr <lucy.vrsovska@gmail.com>
2020-06-01 01:35:00 +0200

    Connect to db, create Post model, routes and controllers

--- 16-node-with-rest-api-basics/app.js
+++ 16-node-with-rest-api-basics/app.js
@@ -1,20 +1,40 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
+
+const MONGODB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-vo9l7.mongodb.net/messages`;
 
 const app = express();
 
 app.use(bodyParser.json()); // application/json, parsing json from incoming requests, stored in req.body
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 // to overwrite default CORS setting
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*'); // allow specific origins to access our data, * wildcard for all origins
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE'); // allow these http methods
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  ); // allow these http methods
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // allow to set content type and auth
   next();
 });
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+  console.log(error, 'error');
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
--- 16-node-with-rest-api-basics/controllers/feed.js
+++ 16-node-with-rest-api-basics/controllers/feed.js
@@ -1,19 +1,61 @@
+const { validationResult } = require('express-validator');
+
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post.' }],
-  });
+  Post.find()
+    .then((posts) => {
+      res.status(200).json({
+        message: 'Fetched posts successfully',
+        posts: posts,
+      });
+    })
+    .catch((err) => next(err));
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect.');
+    error.statusCode = 422;
+    throw error;
+  }
   const title = req.body.title;
   const content = req.body.content;
-  // create post in db
-  res.status(201).json({ // success and created a resource
-    message: 'Post created successfully',
-    post: {
-      id: new Date().toISOString().replace(/:/g, '-'),
-      title: title,
-      content: content,
-    },
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: 'images/fleabag.jpg',
+    creator: { name: 'Lucie' },
   });
+  post
+    .save()
+    .then((postData) => {
+      res.status(201).json({
+        // success and created a resource
+        message: 'New post successfully created.',
+        post: postData,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        // if no status code set yet, set it to 500
+        err.statusCode = 500;
+      }
+      next(err); // continue to error handling middleware
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error('Could not find post.');
+        error.statusCode = 404;
+        throw error; // this error will be passed to catch block and then to middleware error handling func
+      }
+      res.status(200).json({ message: 'Post fetched', post: post });
+    })
+    .catch((err) => next(err));
 };
